574b11a806d755551c22566e399ff9d39b50c7a7
KimHyunJun <kdhc7777@naver.com>
2026-05-28 14:31:51 +0900

    feat(product): add product CRUD APIs

--- src/main/java/com/backend/shoppingmall/controller/ProductController.java
+++ src/main/java/com/backend/shoppingmall/controller/ProductController.java
@@ -2,13 +2,15 @@ package com.backend.shoppingmall.controller;
 
 
 import com.backend.shoppingmall.dto.product.ProductCreateRequest;
+import com.backend.shoppingmall.dto.product.ProductResponse;
+import com.backend.shoppingmall.dto.product.ProductUpdateRequest;
+import com.backend.shoppingmall.entity.Product;
 import com.backend.shoppingmall.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +20,29 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public Long createProduct(@Valid @RequestBody  ProductCreateRequest request) {
+    public Long createProduct(@Valid @RequestBody ProductCreateRequest request) {
         return productService.createProduct(request.getName(), request.getPrice());
+    }
+
+    @GetMapping("/{productId}")
+    public ProductResponse findProduct(@PathVariable Long productId) {
+        return new ProductResponse(productService.findProduct(productId));
+    }
+
+    @GetMapping
+    public List<ProductResponse> findProducts() {
+        return productService.findProducts().stream()
+                .map(ProductResponse::new)
+                .toList();
+    }
+
+    @PatchMapping("/{productId}")
+    public void updateProduct(@PathVariable Long productId, @Valid @RequestBody ProductUpdateRequest request) {
+        productService.updateProduct(productId, request.getName(), request.getPrice());
+    }
+
+    @DeleteMapping("/{productId}")
+    public void deleteProduct(@PathVariable Long productId) {
+        productService.deleteProduct(productId);
     }
 }
