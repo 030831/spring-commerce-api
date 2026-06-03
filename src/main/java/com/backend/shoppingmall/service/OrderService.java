86bfc80dbec553081a651597cdddf0b67c81f7e8
030831 <kdhc7777@naver.com>
2026-06-03 20:49:20 +0900

    feat(order): add order service

---
+++ src/main/java/com/backend/shoppingmall/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.backend.shoppingmall.service;
+
+import com.backend.shoppingmall.entity.Member;
+import com.backend.shoppingmall.entity.Order;
+import com.backend.shoppingmall.entity.OrderItem;
+import com.backend.shoppingmall.entity.Product;
+import com.backend.shoppingmall.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberService memberService;
+    private final ProductService productService;
+
+    @Transactional
+    public Long createOrder(Long memberId, Long productId, int quantity) {
+        Member member = memberService.findMember(memberId);
+        Product product = productService.findProduct(productId);
+        Order order = new Order(member);
+        OrderItem orderItem = new OrderItem(product, quantity);
+
+        order.addOrderItem(orderItem);
+        orderRepository.save(order);
+        return order.getId();
+    }
+}
