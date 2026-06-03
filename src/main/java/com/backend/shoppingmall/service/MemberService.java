1325ebca15f1b5716beeb2e9fa0cafecaacf8e4d
KimHyunJun <kdhc7777@naver.com>
2026-06-03 13:11:37 +0900

    feat(member): add member service

---
+++ src/main/java/com/backend/shoppingmall/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.backend.shoppingmall.service;
+
+import com.backend.shoppingmall.entity.Member;
+import com.backend.shoppingmall.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long createMember(String name, String email) {
+        Member member = new Member(name, email);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
