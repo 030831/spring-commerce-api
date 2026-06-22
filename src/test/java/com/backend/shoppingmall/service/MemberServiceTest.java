3b602cf969c3843996abae82dc4684ad870e8616
030831 <kdhc7777@naver.com>
2026-06-22 19:10:54 +0900

    test(member): add member update service test

--- src/test/java/com/backend/shoppingmall/service/MemberServiceTest.java
+++ src/test/java/com/backend/shoppingmall/service/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package com.backend.shoppingmall.service;
 
 import com.backend.shoppingmall.entity.Member;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,9 @@ class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void 회원_생성_테스트() {
@@ -41,5 +45,23 @@ class MemberServiceTest {
         // then
         assertThat(name).isEqualTo(member.getName());
         assertThat(email).isEqualTo(member.getEmail());
+    }
+
+    @Test
+    void 회원_정보_수정_테스트() {
+        // given
+        Long memberId = memberService.createMember("홍길동", "test@gmail.com");
+
+        // when
+        String updateName = "김민수";
+        String updateEmail = "test2@gmail.com";
+        memberService.updateMember(memberId, updateName, updateEmail);
+        em.flush();
+        em.clear();
+
+        // then
+        Member findMember = memberService.findMember(memberId);
+        assertThat(updateName).isEqualTo(findMember.getName());
+        assertThat(updateEmail).isEqualTo(findMember.getEmail());
     }
 }
